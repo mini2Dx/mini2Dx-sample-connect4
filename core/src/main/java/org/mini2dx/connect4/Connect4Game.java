03fc5e2628def7b65df54d7b91abd10ac82fce53
CaoimheATiernan <51082857+CaoimheATiernan@users.noreply.github.com>
2019-06-04 10:53:46 +0100

    Added Player input. Player uses left,right,up,down to pick tile and the Enter to change colour
    Turn switches after a player picks a tile, removed the up and down input handler, Tiles "fall" to bottom of board.
    Turn switches after a player picks a tile, removed the up and down input handler, Tiles "fall" to bottom of board.

--- core/src/main/java/org/mini2dx/connect4/Connect4Game.java
+++ core/src/main/java/org/mini2dx/connect4/Connect4Game.java
@@ -1,5 +1,4 @@
 package org.mini2dx.connect4;
-import com.badlogic.gdx.graphics.Texture;
 import org.mini2Dx.core.game.BasicGame;
 import org.mini2Dx.core.graphics.Graphics;
 import static org.mini2dx.connect4.Tile.TileColour.BLUE;
@@ -9,7 +8,6 @@ import static org.mini2dx.connect4.Tile.TileColour.RED;
 
 public class Connect4Game extends BasicGame {
 	public static final String GAME_IDENTIFIER = "org.mini2dx.connect4";
-	private Tile rTile, bTile;
 	private Board board;
 	
 	@Override
@@ -17,17 +15,11 @@ public class Connect4Game extends BasicGame {
 
 		board = new Board();
 		board.initialise();
-		rTile = new Tile(RED);
-		rTile.initialise();
-		bTile = new Tile(BLUE);
-		bTile.initialise();
 
     }
     
     @Override
     public void update(float delta) {
-		board.addTileAt(rTile.clone(), 5);
-		board.addTileAt(bTile.clone(), 5);
 		board.update(delta);
     }
     
